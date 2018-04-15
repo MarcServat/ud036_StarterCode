ae3f7815db26544d383492401fa61d51e4cabe4f
MarcServat <marcservat@hotmail.com>
2018-04-15 21:00:30 +0200

    remove white space

--- movie.py
+++ movie.py
@@ -3,8 +3,7 @@ class Movie():
     Attributes:
         title (str): The original `title`.
         poster_image_url (str): The poster width: 185 height: 278.
-        trailer_youtube_url (str): Youtube link to trailer. 
-        """
+        trailer_youtube_url (str): Youtube link to trailer. """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
